468e0e9b21b7f6c1bc34278985c2ba8b258a1102
ArtemS <etogramm82@mail.ru>
2022-01-30 00:33:16 +0300

    fasfqwrfas

--- javascript.js
+++ javascript.js
@@ -389,4 +389,6 @@ function validate(password) {
   return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{6,}$/.test(password);
   }
 
-  
+  function digital_root(n) {
+    return (n - 1) % 9 + 1;
+    }
